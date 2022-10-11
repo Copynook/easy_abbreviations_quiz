b2c3b95fcd958aacb4554804c0435bdf98acfc58
Vasi_lena <105110262+Copynook@users.noreply.github.com>
2022-10-11 22:47:10 +0300

    Update basic_abbreviations_quiz.py

--- basic_abbreviations_quiz.py
+++ basic_abbreviations_quiz.py
@@ -1,5 +1,6 @@
 print("Welcome to my computer quiz!")
-playing = input("Do you want to play?")
+
+playing = input("Do you want to play? ")
 
 if playing.lower() != "yes":
     quit()
@@ -7,26 +8,26 @@ if playing.lower() != "yes":
 print("Okay! Let's play :)")
 score = 0
 
-answer = input("What does CPU stands for?")
+answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
-    print("Correct!")
+    print('Correct!')
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What does CAD stands for?")
+answer = input("What does CAD stands for? ")
 if answer.lower() == "computer aided design":
-    print("Correct!")
+    print('Correct!')
     score += 1
 else:
-    print("Incorrect!")
+    print('Incorrect!')
 
-answer = input("What does RDP stands for?")
+answer = input("What does RDP stands for? ")
 if answer.lower() == "remote desktop protocol":
-    print("Correct!")
+    print('Correct!')
     score += 1
 else:
-    print("Incorrect!")
+    print('Incorrect!')
 
 print("You got " + str(score) + " questions correct!")
 print("You got " + str((score/4) * 100) + "%.")
